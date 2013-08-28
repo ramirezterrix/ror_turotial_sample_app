92e20b8a0458bb2094fd08c44153d3b05c9d65fc
ramirezterrix <ramirezterrix@googlemail.com>
2013-08-28 09:19:52 +0200

    Tests and about Page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  #Addin Capybara DSL to Rspec helper file
+  config.include Capybara::DSL
 end
